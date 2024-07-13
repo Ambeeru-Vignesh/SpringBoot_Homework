3cdc11deef8fef03273fb149bd8b2e28bd573ad5
Ambeeru Vignesh <vigneshvj53@gmail.com>
2024-07-13 20:48:42 +0530

    Properties added

--- src/main/java/SpringBoot/Homework1/ChocolateFrosting.java
+++ src/main/java/SpringBoot/Homework1/ChocolateFrosting.java
@@ -1,10 +1,12 @@
 package SpringBoot.Homework1;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@ConditionalOnProperty(name = "deploy.env.frosting", havingValue = "chocolate")
 public class ChocolateFrosting implements Frosting {
 
     public String getFrostingType(){
--- src/main/java/SpringBoot/Homework1/ChocolateSyrup.java
+++ src/main/java/SpringBoot/Homework1/ChocolateSyrup.java
@@ -1,10 +1,12 @@
 package SpringBoot.Homework1;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@ConditionalOnProperty(name = "deploy.env.syrup", havingValue = "chocolate")
 public class ChocolateSyrup implements Syrup{
 
     public String getSyrupType(){
